7e6f72bca732264fed66555117c781658f784005
fsm13 <fomichevsm@gmail.com>
2023-04-24 16:47:34 +0400

    add MainActivity layout

--- app/src/main/java/my/newapp/fsm/gpstracker/MainActivity.kt
+++ app/src/main/java/my/newapp/fsm/gpstracker/MainActivity.kt
@@ -2,10 +2,28 @@ package my.newapp.fsm.gpstracker
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
+import my.newapp.fsm.gpstracker.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        onClickBNav()
+    }
+
+
+    private fun onClickBNav() {
+        binding.bNav.setOnItemSelectedListener {
+            when(it.itemId) {
+                R.id.id_home -> {}
+                R.id.id_tracks -> {}
+                R.id.id_settings -> {}
+            }
+            true
+        }
     }
 }
