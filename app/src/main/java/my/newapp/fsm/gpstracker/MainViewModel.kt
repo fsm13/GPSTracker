bd7b2e28bc816d47e7f0ffdf87f0e83ddd826be7
fsm13 <fomichevsm@gmail.com>
2023-05-18 12:15:02 +0300

    add mainDB in VM

--- app/src/main/java/my/newapp/fsm/gpstracker/MainViewModel.kt
+++ app/src/main/java/my/newapp/fsm/gpstracker/MainViewModel.kt
@@ -2,11 +2,32 @@ package my.newapp.fsm.gpstracker
 
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.ViewModelProvider
+import androidx.lifecycle.asLiveData
+import androidx.lifecycle.viewModelScope
+import kotlinx.coroutines.launch
+import my.newapp.fsm.gpstracker.db.MainDB
+import my.newapp.fsm.gpstracker.db.TrackItem
 import my.newapp.fsm.gpstracker.location.LocationModel
+import java.lang.IllegalArgumentException
 
-class MainViewModel : ViewModel(){
-
+@Suppress("UNCHECKED_CAST")
+class MainViewModel(db: MainDB) : ViewModel() {
+    val dao = db.getDao()
     val locationUpdates = MutableLiveData<LocationModel>()
     val timeData = MutableLiveData<String>()
+    val tracks = dao.getAllTracks().asLiveData()
 
+    fun insertTrack(trackItem: TrackItem) = viewModelScope.launch {
+        dao.insertTrack(trackItem)
+    }
+
+    class ViewModelFactory(private val db: MainDB) : ViewModelProvider.Factory{
+        override fun <T : ViewModel> create(modelClass: Class<T>): T {
+            if (modelClass.isAssignableFrom(MainViewModel::class.java)) {
+                return MainViewModel(db) as T
+            }
+            throw IllegalArgumentException("Unknown ViewModel class")
+        }
+    }
 }
